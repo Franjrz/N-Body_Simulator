91ab812fd7e4248f247d0e2b679e136dab8458fa
Franjrz <franciscoruedazaragoza@gmail.com>
2021-10-23 01:45:01 +0200

    CMake y pruebas

---
+++ build/CMakeFiles/sim-aos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim-aos.dir/sim-aos.cpp.o"
+  "sim-aos"
+  "sim-aos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim-aos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
